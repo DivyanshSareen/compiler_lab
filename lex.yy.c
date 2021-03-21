54d936f2390f8060744556f52663d1fb6a9b5eb9
DivyanshSareen <sareendivyansh@gmail.com>
2021-03-21 09:46:20 +0530

    q7 and q8 ig

--- lex.yy.c
+++ lex.yy.c
@@ -360,20 +360,20 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[8] =
+static const flex_int16_t yy_accept[11] =
     {   0,
-        0,    0,    4,    2,    1,    1,    0
+        0,    0,    4,    2,    3,    2,    2,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    2,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    3,    4,
+        1,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +397,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[6] =
     {   0,
-        1,    2,    1
+        1,    1,    2,    1,    1
     } ;
 
-static const flex_int16_t yy_base[9] =
+static const flex_int16_t yy_base[14] =
     {   0,
-        0,    0,    5,    6,    0,    0,    6,    2
+        0,    3,   13,   14,    0,    7,   14,    1,   14,   14,
+        7,    1,    9
     } ;
 
-static const flex_int16_t yy_def[9] =
+static const flex_int16_t yy_def[14] =
     {   0,
-        7,    1,    7,    7,    8,    8,    0,    7
+       11,   11,   10,   10,   12,   13,   10,   13,   10,    0,
+       10,   10,   10
     } ;
 
-static const flex_int16_t yy_nxt[10] =
+static const flex_int16_t yy_nxt[20] =
     {   0,
-        4,    5,    4,    6,    7,    3,    7,    7,    7
+       10,    5,    7,    6,    5,    9,    6,    4,    4,    8,
+        8,    9,   10,    3,   10,   10,   10,   10,   10
     } ;
 
-static const flex_int16_t yy_chk[10] =
+static const flex_int16_t yy_chk[20] =
     {   0,
-        1,    1,    1,    8,    3,    7,    7,    7,    7
+        0,    1,   12,    1,    2,    8,    2,   11,   11,   13,
+       13,    6,    3,   10,   10,   10,   10,   10,   10
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,11 +440,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "q6.l"
-#line 2 "q6.l"
+#line 1 "q8.l"
+#line 2 "q8.l"
 #include<stdio.h>
-#line 443 "lex.yy.c"
-#line 444 "lex.yy.c"
+#line 447 "lex.yy.c"
+#line 448 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -657,9 +661,9 @@ YY_DECL
 		}
 
 	{
-#line 5 "q6.l"
+#line 5 "q8.l"
 
-#line 663 "lex.yy.c"
+#line 667 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -686,13 +690,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 11 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 14 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -717,22 +721,23 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
+/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 6 "q6.l"
-fprintf(yyout," ");		
+#line 6 "q8.l"
+fprintf(yyout,"%s\n",yytext);
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 7 "q6.l"
-fprintf(yyout,"%s",yytext);	
+#line 7 "q8.l"
+
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 8 "q6.l"
+#line 8 "q8.l"
 ECHO;
 	YY_BREAK
-#line 736 "lex.yy.c"
+#line 741 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1029,7 +1034,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 11 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1057,11 +1062,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 11 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 10);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1737,7 +1742,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 8 "q6.l"
+#line 8 "q8.l"
 
 
 int yywrap()
@@ -1745,10 +1750,10 @@ int yywrap()
 return 1;
 }
 
-int main(void)
+int main()
 {
-yyin=fopen("Input.txt","r");		
-yyout=fopen("Output.txt","w");
+yyin=fopen("input.html","r");
+yyout=fopen("output.txt","w");
 yylex();
 return 0;
 }
---
+++ q7/Input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>  
+
+// sample program code with single and multiline comments
+
+int main(){    
+    /*printing information   
+      Multi-Line Comment*/  
+    printf("Hello C");    
+return 0;  
+}       
